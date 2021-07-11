d3f6afa08afeb4d211a4149a5a59359aabff68e2
Romuald Ekemkingni <romuald.ekemkingni@devoteam.com>
2021-07-11 13:31:18 +0200

    Installed latest bootstrap, popper.js and font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {far} from "@fortawesome/free-regular-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning-angular';
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(far, fas);
+  }
 }
